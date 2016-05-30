776dc5e8bd7ee5ad3881eb0428cc0fa7f89d4c8c
zuev <zuev@scrapinghub.com>
2016-05-30 19:12:39 +0300

    Jobs API update

--- arachnado/handlers.py
+++ arachnado/handlers.py
@@ -8,7 +8,7 @@ from tornado.web import Application, RequestHandler, url, HTTPError
 from arachnado.utils.misc import json_encode
 from arachnado.monitor import Monitor
 from arachnado.handler_utils import ApiHandler, NoEtagsMixin
-from arachnado.rpc import MainRpcHttpHandler, MainRpcWebsocketHandler
+from arachnado.rpc import MainRpcHttpHandler, MainRpcWebsocketHandler, JobsRpcWebsocketHandler, ItemsRpcWebsocketHandler
 
 
 at_root = lambda *args: os.path.join(os.path.dirname(__file__), *args)
@@ -37,6 +37,8 @@ def get_application(crawler_process, domain_crawlers,
         url(r"/ws-updates", Monitor, context, name="ws-updates"),
         url(r"/ws-rpc", MainRpcWebsocketHandler, context, name="ws-rpc"),
         url(r"/rpc", MainRpcHttpHandler, context, name="rpc"),
+        url(r"/ws-jobs", JobsRpcWebsocketHandler, context, name="ws-jobs"),
+        url(r"/ws-items", ItemsRpcWebsocketHandler, context, name="ws-items"),
     ]
     return Application(
         handlers=handlers,
--- arachnado/rpc/__init__.py
+++ arachnado/rpc/__init__.py
@@ -7,6 +7,8 @@ jsonrpc.jdumps = json_encode
 import tornadorpc
 from tornadorpc.json import JSONRPCHandler
 from tornado.concurrent import Future
+from tornado import gen
+import tornado.ioloop
 
 from arachnado.rpc.jobs import JobsRpc
 from arachnado.rpc.sites import SitesRpc
@@ -23,6 +25,7 @@ class MainRpcHttpHandler(JSONRPCHandler):
     """ Main JsonRpc router for REST requests"""
 
     def initialize(self, *args, **kwargs):
+        print("MainRpcHttpHandler init")
         self.jobs = JobsRpc(self, *args, **kwargs)
         self.sites = SitesRpc(self, *args, **kwargs)
         self.pages = PagesRpc(self, *args, **kwargs)
@@ -40,5 +43,107 @@ class MainRpcHttpHandler(JSONRPCHandler):
         self._RPC_.response(self)
 
 
+
+
 class MainRpcWebsocketHandler(JsonRpcWebsocketHandler, MainRpcHttpHandler):
     """ Main JsonRpc router for WS stream"""
+
+
+class JobsRpcWebsocketHandler(MainRpcWebsocketHandler):
+    """ jobs info for WS stream"""
+    job_info = {}
+    delay_mode = False
+    job_event_type = 'jobs.tailed'
+    job_hb = None
+
+    @gen.coroutine
+    def write_event(self, event, data):
+        print("write_event!!!!!!!!!!!!!")
+        if event == self.job_event_type and self.delay_mode:
+            self.job_info[data["id"]] = data
+        else:
+            return super(MainRpcWebsocketHandler, self).write_event(event, data)
+
+    def subscribe_to_jobs(self, include=[], exclude=[], update_delay=0):
+        print("subscribe_to_jobs!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(include)
+        print(exclude)
+        conditions = []
+        for inc_str in include:
+            conditions.append({"urls":{'$regex': '.*' + inc_str + '.*'}})
+        for exc_str in exclude:
+            conditions.append({"urls":{'$regex': '^((?!' + exc_str + ').)*$'}})
+        jobs_q = {}
+        if len(conditions) == 1:
+            jobs_q = conditions[0]
+        elif len(conditions):
+            jobs_q = {"$and": conditions }
+        if update_delay > 0:
+            self.delay_mode = True
+            self.job_hb = tornado.ioloop.PeriodicCallback(
+                lambda: self.send_updates(),
+                update_delay
+            )
+            self.job_hb.start()
+        self.jobs.subscribe(query=jobs_q)
+
+    def initialize(self, *args, **kwargs):
+        print("JobsRpcWebsocketHandler init")
+        self.jobs = JobsRpc(self, *args, **kwargs)
+
+    def send_updates(self):
+        for job_id in self.job_info:
+            res = super(JobsRpcWebsocketHandler, self).write_event(self.job_event_type, self.job_info[job_id])
+            self.job_info.pop(job_id)
+            return res
+
+
+class ItemsRpcWebsocketHandler(MainRpcWebsocketHandler):
+    """ items info for WS stream"""
+    items = []
+    delay_mode = False
+    page_event_type = 'pages.tailed'
+    item_hb = None
+
+    @gen.coroutine
+    def write_event(self, event, data):
+        print("write_event!!!!!!!!!!!!!")
+        if event == self.page_event_type and self.delay_mode:
+            self.items.append(data)
+        else:
+            return super(MainRpcWebsocketHandler, self).write_event(event, data)
+
+    def subscribe_to_items(self, site_ids={}, update_delay=0):
+        print("subscribe_to_items!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(site_ids)
+        # print(exclude)
+        conditions = []
+        for site in site_ids:
+            conditions.append(
+                {"$and":[{"url":{"$regex": site + '.*'}},
+                    {"_id":{"$gt":site_ids[site]}}
+                ]}
+            )
+        items_q = {}
+        if len(conditions) == 1:
+            items_q = conditions[0]
+        elif len(conditions):
+            items_q = {"$or": conditions }
+        if update_delay > 0:
+            self.delay_mode = True
+            self.item_hb = tornado.ioloop.PeriodicCallback(
+                lambda: self.send_updates(),
+                update_delay
+            )
+            self.item_hb.start()
+        self.pages.subscribe(query=items_q)
+
+    def initialize(self, *args, **kwargs):
+        print("ItemsRpcWebsocketHandler init")
+        self.pages = PagesRpc(self, *args, **kwargs)
+
+    def send_updates(self):
+        for item in self.items:
+            self.items.remove(item)
+            return super(ItemsRpcWebsocketHandler, self).write_event(self.page_event_type, item)
+
