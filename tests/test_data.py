a31e389d825fd1be987dae98532f454bafc9d5e8
zuev <zuev@scrapinghub.com>
2016-06-08 20:18:46 +0300

    unit tests update

--- tests/test_data.py
+++ tests/test_data.py
@@ -9,8 +9,17 @@ import tests.utils as u
 class TestJobsAPI(tornado.testing.AsyncHTTPTestCase):
     ws_uri = r"/ws-data"
 
+    def setUp(self):
+        print("setUp:")
+        tornado.ioloop.IOLoop.current().run_sync(u.init_db)
+        super(TestJobsAPI, self).setUp()
+
     def get_app(self):
         return u.get_app(self.ws_uri)
+
+    # @tornado.testing.gen_test
+    # def test_fail(self):
+    #     self.assertTrue(False)
 
     @tornado.testing.gen_test
     def test_jobs_no_filter(self):
@@ -31,6 +40,36 @@ class TestJobsAPI(tornado.testing.AsyncHTTPTestCase):
         json_response = json.loads(response)
         print(json_response)
         self.assertTrue("id" in json_response.get("data", {}))
+        self.execute_cancel(ws_client, json_response.get("data", {}).get("id", -1), True)
+
+    @tornado.testing.gen_test
+    def test_jobs_filter_include(self):
+        jobs_command = {
+            'event': 'rpc:request',
+            'data': {
+                'id':0,
+                'jsonrpc': '2.0',
+                'method': 'subscribe_to_jobs',
+                'params': {
+                    "include":["127.0.0.1"],
+                },
+            },
+        }
+        ws_url = "ws://localhost:" + str(self.get_http_port()) + self.ws_uri
+        ws_client = yield tornado.websocket.websocket_connect(ws_url)
+        ws_client.write_message(json.dumps(jobs_command))
+        response = yield ws_client.read_message()
+        json_response = json.loads(response)
+        print(json_response)
+        self.assertTrue("id" in json_response.get("data", {}))
+        cnt = 0
+        while cnt < 1:
+            response = yield ws_client.read_message()
+            json_response = json.loads(response)
+            if json_response is None:
+                self.assertFail()
+                break
+            cnt += 1
         self.execute_cancel(ws_client, json_response.get("data", {}).get("id", -1), True)
 
     @tornado.testing.gen_test
--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
 import tornado
 import json
 from arachnado.rpc.data import DataRpcWebsocketHandler
 from arachnado.storages.mongotail import MongoTailStorage
+from arachnado.utils.mongo import motor_from_uri
+
+def get_db_uri():
+    return "mongodb://localhost:27017/arachnado-test"
 
 
 def get_app(ws_uri):
-    db_uri = "mongodb://localhost:27017/arachnado"
+    db_uri = get_db_uri()
     items_uri = "{}/items".format(db_uri)
     jobs_uri = "{}/jobs".format(db_uri)
     job_storage = MongoTailStorage(jobs_uri, cache=True)
@@ -20,4 +25,24 @@ def get_app(ws_uri):
         (ws_uri, DataRpcWebsocketHandler, context)
     ])
     return app
+
+
+@tornado.gen.coroutine
+def init_db():
+    db_uri = get_db_uri()
+    # items_uri = "{}/items".format(db_uri)
+    uri = "{}/jobs".format(db_uri)
+    in_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "jobs.jl")
+    _, _, _, _, col = motor_from_uri(uri)
+    col_cnt = yield col.count()
+    print(col_cnt)
+    col.drop()
+    col_cnt = yield col.count()
+    print(col_cnt)
+    with open(in_path, "r") as fin:
+        for text_line in fin:
+            job = json.loads(text_line)
+            print(job["_id"])
+            res = yield col.insert(job)
+
 
